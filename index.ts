ffc2af967062452e4c71e1804399c40ff29d0ebd
David <d.elbaze@gmail.com>
2022-09-20 21:55:39 +0200

    compilation

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ dotenv.config();
 import express from "express";
 import datasource from "./lib/datasource";
 import {wilders, languages} from "./routes";
-
 const app = express()
 
 app.use(express.urlencoded({ extended: true}));
